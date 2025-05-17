f6c2431544c1c133980a2f168ac23721ff70eb9f
Ashhiii <delapena.jefferson11@gmail.com>
2025-05-18 03:19:24 +0800

    render

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from .views import create_user
 from . import views
 
 urlpatterns = [
-    path('create-user/', create_user, name='create_user'),
-    path('login-user/', views.login_user, name='login_user'),
+    path('create-user', create_user, name='create_user'),
+    path('login-user', views.login_user, name='login_user'),
     path('create-admin/', views.create_admin),
     path('login-admin/', views.login_admin),
 ]
